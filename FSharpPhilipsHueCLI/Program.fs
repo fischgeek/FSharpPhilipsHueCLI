a63f0b51190c116a41f9b66547334489b9961c87
fischgeek <fischgeek@gmail.com>
2020-02-27 17:23:06 -0600

    initial commit

---
+++ FSharpPhilipsHueCLI/Program.fs
@@ -0,0 +1,49 @@
+﻿// Learn more about F# at http://fsharp.org
+open System
+open System.IO
+open FSharpCoreLib
+open Utf8Json
+open FSharp.Data
+
+//type Config = {
+//    BaseUrl: string
+//    BridgeIP: string
+//    Username: string
+//}
+type Config = JsonProvider<"sample_config.json">
+
+//let content = Config.Parse(File.ReadAllText(""))
+
+let configFile = @"config.json"
+
+//let rec GetConfig () : string = 
+//    let c = configFile |> FileOps.ReadAllTextIfExists
+//    match c with
+//    | Some (c) -> c
+//    | None -> 
+//        configFile |> FileOps.Create
+//        GetConfig()
+
+//let ConvertConfigIntoObject configContents = Config.Parse(configContents)
+
+//let superGetConfig =
+//      let config = GetConfig ()
+//      printfn "Config file says: %s" config
+//      let x = config |> ConvertConfigIntoObject
+      
+//      {|
+//          BaseConfig = x
+//          calculatedUrl = x.BaseUrl + x.BridgeIp
+//      |}
+
+let wait() = Console.ReadLine() |> ignore
+let out (x: obj) = System.Console.WriteLine(x.ToString())
+[<EntryPoint>]
+let main argv =
+    //printfn "IP = %s" superGetConfig.BaseConfig.BridgeIp
+    //printfn "%s" superGetConfig.calculatedUrl
+
+    //SharedFSharpVanilla.Pipes.StringPipe.ConcatSpaces [|"hello";"how";"are";"you"|] |> out
+
+    wait()
+    0
